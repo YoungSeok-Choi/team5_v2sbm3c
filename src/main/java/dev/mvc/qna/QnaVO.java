6616cba73a5b481c755d425d99bd6fba86e84a96
YoungSeok-Choi <dydtjr1717@gmail.com>
2021-12-14 13:45:34 +0900

    VO 추가 및 oracle local config

--- src/main/java/dev/mvc/qna/QnaVO.java
+++ src/main/java/dev/mvc/qna/QnaVO.java
@@ -1,5 +1,32 @@
 package dev.mvc.qna;
 
-public class QnaVO {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter @Setter @ToString
+public class QnaVO {
+    
+    private int qnano;
+    private String title = ""; // for avoiding NPE
+    private String secret = "";
+    private String text = ""; 
+    private String qdate = ""; 
+    private int qcateno;
+    
+    
+    public QnaVO(int qnano, String title, String secret, String text, String qdate, int qcateno) {
+        this.qnano = qnano;
+        this.title = title;
+        this.secret = secret;
+        this.text = text;
+        this.qdate = qdate;
+        this.qcateno = qcateno;
+    }
+    
+    public QnaVO () {
+        
+    }
+    
+    
 }
